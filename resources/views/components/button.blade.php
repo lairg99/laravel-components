067a03cb86f8f9cfeb5a125a9dd871be35b20e46
samnowakowski <sam.nowakowski@sector27.de>
2023-09-22 09:59:58 +0200

    Whitespace nowrap

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,10 +9,10 @@
 ])
 
 @php
-    $baseStyle = 'group transition duration-150 inline-flex items-center justify-center border font-bold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800';
+    $baseStyle = 'group whitespace-nowrap transition duration-150 inline-flex items-center justify-center border font-bold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800';
 
     $colorStyle = match($color) {
-        'primary' =>   'bg-primary-100  border-primary-100  hover:bg-primary-200  text-primary-800  focus:ring-primary-300  dark:bg-primary-800  dark:border-primary-800  dark:hover:bg-primary-700  dark:text-gray-100 dark:focus:ring-primary-700',
+        'primary' =>   'bg-primary-100  border-primary-100  hover:bg-primary-200  text-primary-700  focus:ring-primary-300  dark:bg-primary-800  dark:border-primary-800  dark:hover:bg-primary-700  dark:text-gray-100 dark:focus:ring-primary-700',
         'secondary' => 'bg-gray-200 border-gray-200 hover:bg-gray-300 text-gray-700 focus:ring-gray-400 dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-900 dark:text-gray-100 dark:focus:ring-gray-700',
         'tertiary' =>  'bg-none     border-none     hover:bg-gray-200 text-gray-600 focus:ring-gray-300 dark:none        dark:border-none     dark:hover:bg-gray-900 dark:text-gray-100 dark:focus:ring-gray-600',
         'neutral' =>   'bg-white    border-white    hover:bg-gray-100 text-gray-600 focus:ring-gray-300 dark:bg-gray-900 dark:border-gray-900 dark:hover:bg-gray-900 dark:text-gray-100 dark:focus:ring-gray-600',
