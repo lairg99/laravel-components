6bbd758aa66026d2bedaec96725d92a8f4fe12ec
Sam Nowakowski <samnowi@me.com>
2023-10-15 14:07:49 +0200

    Feedback dark mode, added badge

---
+++ resources/views/components/badge.blade.php
@@ -0,0 +1,30 @@
+@props(['color' => 'primary', 'size' => null, 'loading' => false])
+
+@php
+    $colorStyle = match($color) {
+        'primary', 'red'                 => 'bg-red-200    text-red-900    dark:bg-red-800    dark:text-red-100',
+        'secondary', 'tertiary', 'gray'  => 'bg-gray-200   text-gray-700   dark:gray-900      dark:text-gray-800',
+        'green', 'lime'                  => 'bg-lime-200   text-lime-900   dark:bg-lime-700   dark:text-lime-100',
+        'yellow', 'amber'                => 'bg-amber-200  text-amber-800  dark:bg-amber-600  dark:text-amber-100',
+        'orange'                         => 'bg-orange-200 text-orange-800 dark:bg-orange-600 dark:text-orange-100',
+        'blue', 'sky'                    => 'bg-sky-200    text-sky-800    dark:bg-sky-700    dark:text-sky-100',
+        'purple'                         => 'bg-purple-200 text-purple-800 dark:bg-purple-600 dark:text-purple-100',
+        'pink'                           => 'bg-pink-200   text-pink-800   dark:bg-pink-600   dark:text-pink-100',
+    };
+
+    $sizeStyle = match($size) {
+        'sm' => 'px-2 py-0 text-2xs',
+        default => 'px-2.5 py-0.5 text-xs'
+    };
+
+@endphp
+
+<div {{ $attributes->merge(['class' => "inline-flex justify-center font-bold rounded-full whitespace-nowrap {$sizeStyle} {$colorStyle}"]) }}>
+    {{ $slot }}
+
+    @if($loading)
+        <div class="ml-1 -mt-[1px]">
+            <i class="fa-solid fa-spinner text-2xs animate-spin"></i>
+        </div>
+    @endif
+</div>
--- resources/views/components/feedback.blade.php
+++ resources/views/components/feedback.blade.php
@@ -4,10 +4,10 @@
     $type ??= 'success';
 
     $color = match($type) {
-        'danger' =>  'bg-red-100   text-red-800   dark:bg-red-700   dark:text-red-100',
-        'success' => 'bg-lime-100  text-lime-800  dark:bg-lime-700  dark:text-lime-100',
-        'warning' => 'bg-amber-100 text-amber-800 dark:bg-amber-700 dark:text-amber-100',
-        'info' =>    'bg-gray-200  text-gray-700  dark:bg-gray-700  dark:text-gray-100',
+        'danger' =>  'bg-red-100   text-red-800   border-red-200   dark:bg-red-900   dark:text-red-50 dark:border-red-700',
+        'success' => 'bg-lime-100  text-lime-800  border-lime-200  dark:bg-lime-800  dark:text-lime-50 dark:border-lime-600',
+        'warning' => 'bg-amber-100 text-amber-800 border-amber-200 dark:bg-amber-800 dark:text-amber-50 dark:border-amber-600',
+        'info' =>    'bg-gray-200  text-gray-700  border-gray-300  dark:bg-gray-800  dark:text-gray-100 dark:border-gray-500',
     };
 
     $iconColor = match($type) {
@@ -34,7 +34,7 @@
     };
 @endphp
 
-<div {{ $attributes->merge(['class' => "rounded-md {$color} {$size} flex items-start"]) }}>
+<div {{ $attributes->merge(['class' => "rounded-md border {$color} {$size} flex items-start"]) }}>
     @if($icon !== 'none')
         <i class="fa-solid inline-block fa-{{ $icon }} mt-1 {{ $iconColor }} {{ $iconSize }}"></i>
     @endif
