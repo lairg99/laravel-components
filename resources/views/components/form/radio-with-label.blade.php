d8d7a095166cbc14b8b0f503bb8b9862b4aab806
samnowakowski <sam.nowakowski@sector27.de>
2023-11-02 09:55:23 +0100

    radio with label

---
+++ resources/views/components/form/radio-with-label.blade.php
@@ -0,0 +1,9 @@
+<label {{ $attributes->except(['wire:model', 'value'])->merge(['class' => 'flex items-start']) }}>
+    <x-veo::form.radio class="mr-4 mt-[4px]" {{ $attributes->only(['wire:model', 'wire:model.defer', 'value']) }}/>
+
+    @if($slot->isNotEmpty())
+        <div class="text-gray-600 dark:text-gray-300">
+            {{ $slot }}
+        </div>
+    @endif
+</label>
