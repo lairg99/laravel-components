9c538cd2ffc881d7bdbdafd8e1bf7c9e0b778809
samnowakowski <sam.nowakowski@sector27.de>
2023-11-02 10:27:04 +0100

    coloring

--- resources/views/components/form/radio.blade.php
+++ resources/views/components/form/radio.blade.php
@@ -2,5 +2,5 @@
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'type' => 'radio',
-    'class' => 'full-rounded border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 dark:bg-gray-900 dark:border-gray-800 dark:focus:ring-gray-900 dark:focus:border-gray-600 dark:focus:ring-offset-gray-900 focus:ring-opacity-50'
+    'class' => 'full-rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 dark:bg-gray-900 dark:border-gray-800 dark:focus:ring-gray-900 dark:focus:border-gray-600 dark:focus:ring-offset-gray-900 focus:ring-opacity-50'
 ]) !!}>
