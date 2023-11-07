08e41e0d62cd2b6e73d9ea077a82189cb2195646
samnowakowski <sam.nowakowski@sector27.de>
2023-11-07 08:44:41 +0100

    Name

--- resources/views/components/form/radio-with-label.blade.php
+++ resources/views/components/form/radio-with-label.blade.php
@@ -1,5 +1,5 @@
 <label {{ $attributes->except(['wire:model', 'value'])->merge(['class' => 'flex items-start']) }}>
-    <x-veo::form.radio class="mr-4 mt-[4px]" {{ $attributes->only(['wire:model', 'wire:model.defer', 'value']) }}/>
+    <x-veo::form.radio class="mr-4 mt-[4px]" {{ $attributes->only(['wire:model', 'wire:model.defer', 'value', 'name']) }}/>
 
     @if($slot->isNotEmpty())
         <div class="text-gray-600 dark:text-gray-300">
