d9c0f967fa72996c143339423c9286ff4252b3a4
Sam Nowakowski <samnowi@me.com>
2023-12-26 22:17:03 +0100

    Dark mode

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -4,7 +4,7 @@
     $colorStyle = match($color) {
         'primary', 'red'                 => 'bg-primary-200 text-primary-900 dark:bg-primary-800 dark:text-primary-100',
         'red'                            => 'bg-red-200     text-red-900     dark:bg-red-800     dark:text-red-100',
-        'secondary', 'tertiary', 'gray'  => 'bg-gray-200    text-gray-700    dark:gray-900       dark:text-gray-800',
+        'secondary', 'tertiary', 'gray'  => 'bg-gray-200    text-gray-700    dark:bg-gray-900    dark:text-gray-200',
         'green', 'lime'                  => 'bg-lime-200    text-lime-900    dark:bg-lime-700    dark:text-lime-100',
         'yellow', 'amber'                => 'bg-amber-200   text-amber-800   dark:bg-amber-600   dark:text-amber-100',
         'orange'                         => 'bg-orange-200  text-orange-800  dark:bg-orange-600  dark:text-orange-100',
