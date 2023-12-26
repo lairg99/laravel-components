a6c4e2b78c3113f8b8c5586751b938ff70270d61
Sam Nowakowski <samnowi@me.com>
2023-12-26 22:12:26 +0100

    Fixed responsive design

--- resources/views/components/h1.blade.php
+++ resources/views/components/h1.blade.php
@@ -19,10 +19,10 @@
     </div>
 
     @if($withSearch || $filters)
-        <div @class(['flex flex-col sm:items-center sm:flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 sm:pl-5 md:ml-5 sm:ml-auto', 'md:border-l dark:border-gray-700' => $actions !== null])>
+        <div @class(['flex items-center flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 md:pl-5 md:ml-5', 'md:border-l dark:border-gray-700' => $actions !== null])>
             @if($withSearch)
-                <div>
-                    <x-veo::form.input wire:model="search" input-class="py-1.5" class="md:max-w-[200px]" with-spinner/>
+                <div class="-my-4 flex-1">
+                    <x-veo::form.input wire:model="search" class="py-1.5" placeholder="Suchen ..." with-spinner/>
                 </div>
             @endif
 
--- resources/views/components/h2.blade.php
+++ resources/views/components/h2.blade.php
@@ -1,6 +1,6 @@
 @props(['actions' => null, 'withSearch' => false, 'filters' => null])
 
-<div {{ $attributes->class(['flex w-full md:items-center flex-col md:flex-row mt-5 sm:mt-7 mb-4 sm:mb-5']) }}>
+<div {{ $attributes->class(['flex w-full md:items-center flex-col md:flex-row mt-5 sm:mt-7 mb-4 sm:mb-5 gap-y-3']) }}>
     <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200">
         {{ $slot }}
     </h2>
@@ -12,7 +12,7 @@
     </div>
 
     @if($withSearch || $filters)
-        <div @class(['flex flex-col sm:items-center sm:flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 md:pl-5 md:ml-5 md:ml-auto', 'md:border-l dark:border-gray-700' => $actions !== null])>
+        <div @class(['flex items-center flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 md:pl-5 md:ml-5', 'md:border-l dark:border-gray-700' => $actions !== null])>
             @if($withSearch)
                 <div class="-my-4 flex-1">
                     <x-veo::form.input wire:model="search" class="py-1.5" placeholder="Suchen ..." with-spinner/>
