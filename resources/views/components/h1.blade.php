65b26fb1fb34bc52076b91213da487d49caa0b55
Sam Nowakowski <samnowi@me.com>
2023-12-29 17:50:45 +0100

    Paddings

--- resources/views/components/h1.blade.php
+++ resources/views/components/h1.blade.php
@@ -1,6 +1,6 @@
 @props(['actions' => null, 'badges' => null, 'withSearch' => false, 'filters' => null])
 
-<div {{ $attributes->class(['flex w-full md:items-center flex-col md:flex-row my-8']) }}>
+<div {{ $attributes->class(['flex w-full md:items-center flex-col md:flex-row my-8 gap-y-3']) }}>
     <div class="flex flex-col lg:flex-row lg:items-center">
         <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-200">
             {{ $slot }}
@@ -13,13 +13,13 @@
     </div>
 
     @if($actions)
-        <div class="sm:ml-auto flex flex-row items-center flex-wrap gap-x-3 gap-y-2 mt-2 sm:mt-0">
+        <div class="sm:ml-auto flex flex-row items-center flex-wrap gap-x-3 gap-y-2">
             {{ $actions }}
         </div>
     @endif
 
     @if($withSearch || $filters)
-        <div @class(['flex items-center flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 md:pl-5 md:ml-auto mb-3 md:mb-0', 'md:border-l dark:border-gray-700' => $actions !== null])>
+        <div @class(['flex items-center flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 md:pl-5 md:ml-5', 'md:border-l dark:border-gray-700' => $actions !== null])>
             @if($withSearch)
                 <div class="-my-4 flex-1">
                     <x-veo::form.input wire:model="search" class="py-1.5" placeholder="Suchen ..." with-spinner/>
