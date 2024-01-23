6e2448b43da53f050df75065d8d7360b5bd87fe1
Sam Nowakowski <samnowi@me.com>
2024-01-23 16:34:51 +0100

    Styling

--- resources/views/components/h2.blade.php
+++ resources/views/components/h2.blade.php
@@ -1,7 +1,7 @@
 @props(['actions' => null, 'withSearch' => false, 'filters' => null])
 
 <div {{ $attributes->class(['flex w-full md:items-center flex-col md:flex-row mt-5 sm:mt-7 mb-4 sm:mb-5 gap-y-3']) }}>
-    <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200">
+    <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200 flex-1">
         {{ $slot }}
     </h2>
 
@@ -15,7 +15,7 @@
         <div @class(['flex items-center flex-row gap-x-3 gap-y-2 mt-3 md:mt-0 md:pl-5 mb-3 md:mb-0 ml-0 md:ml-5', 'md:border-l dark:border-gray-700' => $actions !== null])>
             @if($withSearch)
                 <div class="-my-4 flex-1">
-                    <x-veo::form.input wire:model="search" class="py-1.5" placeholder="Suchen ..." with-spinner/>
+                    <x-veo::form.input wire:model="search" input-class="py-1" placeholder="Suchen ..." with-spinner/>
                 </div>
             @endif
 
